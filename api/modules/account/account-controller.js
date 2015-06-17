67932f4f9f201ba3fb1331827de26796cc668aa4
costa simon <costapasquier.simon@gmail.com>
2015-06-17 23:27:21 +0200

    feat(account): add http.fail instead of return next

--- api/modules/account/account-controller.js
+++ api/modules/account/account-controller.js
@@ -3,17 +3,15 @@ var AccountService = require('./account-service');
 
 module.exports.mount = function (server) {
 	server.post('/api/account/login', function (req, res, next) {
-		if (!req.body || !req.body.user) {
-			res.send(400, 'bad_param');
-			return next();
-		}
+		if (!req.body || !req.body.user) return next(http.fail(400, 'bad_parma'));
 
 		var userBody = req.body.user;
-		if (!userBody.email || !userBody.password) return res.send(400, 'bad_param');
+		if (!userBody.email || !userBody.password) return next(http.fail(400, 'bad_param'));
 
 		AccountService.findUser(userBody.email, function (err, user) {
-			if (err || !user || !user.password) return next(err);
-			if (user.password !== userBody.password) return next(err);
+			if (err) return next(err);
+			if (!user) return next(http.fail(400, 'user_missing'));
+			if (user.password !== userBody.password) return next(http.fail(400, 'bad_password'));
 
 			res.send(200);
 			return next();
